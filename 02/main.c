fab5b09450bf6d2e51ff3b758a53b2c8ee435f24
Aleksandr Charkov <alcharkov@users.noreply.github.com>
2022-08-10 22:20:08 +0300

    Update main.c

--- 02/main.c
+++ 02/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
 
 unsigned setbits(unsigned x, int p, int n, int y) {
   return (x & (~(~0 << n) | (1 << p))) | (y & ~(~0 << n)) << (p - n);
@@ -8,9 +11,15 @@ unsigned invert(unsigned x, int p, int n){
   return x ^ (~(~0 << p + 1 - n) << (p - n));
 }
 
+unsigned int rightrot(unsigned x, int n) {
+	int bits_size = (int) ceil(log2(x));
+	return ((x & (~(~0 << (bits_size - n)) << n)) >> n) | ((x & ~(~0 << n)) << (bits_size + 1 - n));
+}
+
 int main() {
   unsigned int number = 178;
 	printf("Answer:%u\n", setbits(number, 7, 4, 219));
 	printf("Answer invert:%u\n", invert(number, 7, 4));
+	printf("Answer rightrot:%u\n", rightrot(number, 3));
 	return 0;
 }
